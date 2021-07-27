e567aebe50d7cf5a2e9667c19592e787c2867ae0
rnofal1 <rnofal@umich.edu>
2021-07-27 17:18:37 -0400

    Add nicer styling to homepage

---
+++ src/components/container-index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import * as containerStyles from "./container-index.module.css"
+export default function Container({ children }) { //Will sorta wrap around the html to have nice formatting
+    return <div className={containerStyles.container}>{children}</div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,50 @@
 import React from "react"
 import { Link } from "gatsby"; //Needed to traverse between pages
-import Container from "../components/container" //Import the html "wrapper" for formatting
-import Layout from "../components/layout" //Import the default layout to have a link to Index/Main page at the top
+import { header, container, navLinks, navLinkItem, navLinkText }  from "../components/container-index.module.css" 
+//import Layout from "../components/layout" //Import the default layout to have a link to Index/Main page at the top
 import { graphql } from "gatsby" //Will allow us to use common bits of data
+import portrait from "../images/IMG_1685.jpeg"
+
+
 
 export default function Home({ data }) {
-  return (
-    <Layout>
-    <Container>
-    <div style={{ color: `teal` }}>
-      <img src="https://www.wilsoninfo.com/welcome/welcomeclipart10.gif" alt="" />
-      <p>Site Map: </p>
-      <Link to="/home/">Home</Link>
-      <p></p>
-      <Link to="/blog/">Blog</Link>
-    </div>
-    </Container>
-    </Layout>
+    return (
+       
+        <main className={container}>
+            <div className={header}>
+                <img src={portrait} alt="Icon" />
+                <h1>Ramsey Nofal</h1>
+            </div>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/home/" className={navLinkText}>
+                            Home
+            </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog/" className={navLinkText}>
+                            Blog
+            </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog/" className={navLinkText}>
+                            Blog
+            </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog/" className={navLinkText}>
+                            Blog
+            </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog/" className={navLinkText}>
+                            Blog
+            </Link>
+                    </li>
+                </ul>
+            </nav>
+        </main>
   )
 }
 /* Below will retrieve the site title from my metadata in gatsby-config.js */
